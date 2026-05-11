80e981ec9293e1dd0acd4c8823f823c4f3fe4b81
md-yeamin-habib <1989yeasminhabib@gmail.com>
2026-05-11 21:45:33 +0530

    Update script.js

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1590,7 +1590,7 @@ runBtn.onclick = async () => {
     const formattedTime = elapsed < 1000
         ? `${elapsed.toFixed(2)} ms`
         : `${(elapsed / 1000).toFixed(2)} s`;
-    
+    document.getElementById("evaluation-time").textContent = formattedTime;
   } catch (err) {
     if (err.name === "AbortError") {
       console.log("GA run cancelled");
@@ -1599,7 +1599,6 @@ runBtn.onclick = async () => {
     console.error(err);
     alert("Network error while running GA");
   } finally {
-    document.getElementById("evaluation-time").textContent = formattedTime;
     runBtn.disabled = false;
   }
 };
